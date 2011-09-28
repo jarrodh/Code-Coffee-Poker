165225549e6c9cdb5e2e2d2221fadcef6011d3af
Jarrod Hermer <netrunner2@gmail.com>
2011-09-28 22:35:02 +0200

    Updated eq? to check object is of type card
    By default the eq? method compares type and value.
    Added DS_STORE to the gitignore

--- lib/card.rb
+++ lib/card.rb
@@ -7,6 +7,7 @@ class Card
   end
   
   def eql?(other_card)
+    return false unless other_card.kind_of?(self.class)
     @value == other_card.value && @suit == other_card.suit
   end
   alias :== :eql?
